9acf327d0aeb3a20b00a03634476572439dfc97f
임우철 <lminggvick>
2019-11-21 15:52:39 +0900

    springboot init

---
+++ src/test/java/cs/db/house/HouseApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.db.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
